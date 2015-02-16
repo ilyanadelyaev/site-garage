53519651b5952539cf25c32a860da107da85f2d2
ilyanadelyaev <ilyanadelyaev@ilyas-MacBook-Air.local>
2015-02-16 16:52:56 +0300

    fix mailer

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,23 +1,19 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
+if(empty($_POST['name']) || empty($_POST['phone']))
+{
+    echo "No arguments Provided!";
 	return false;
-   }
+}
 	
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'ilya.nadelyaev@honda-ligovsky.ru';
+$to = 'info@honda-ligovsky.ru';
 $email_subject = "Заявка для СТО Лиговская от $name";
-$email_body = "Получена новая заявка для СТО Лиговская.\n\n"."Имя: $name\n\nТелефон: $phone\n\nСообщение:\n$message";
+$email_body = "Получена новая заявка для СТО Лиговская.\n\n"."Имя: $name\nТелефон: $phone\n\nСообщение:$message";
 $headers = "From: info@honda-ligovsky.ru\n";
 mail($to,$email_subject,$email_body,$headers);
 return true;			
